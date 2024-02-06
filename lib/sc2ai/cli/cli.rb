4ac34347315f0f0e84e8ce63bbdfee3ee5789695
dysonreturns <4380544+dysonreturns@users.noreply.github.com>
2024-02-07 01:07:49 +0200

    Update cli.rb
    fix: cli setup410 better Windows support

--- lib/sc2ai/cli/cli.rb
+++ lib/sc2ai/cli/cli.rb
@@ -1,4 +1,6 @@
 require "thor"
+
+
 require "sc2ai/cli/new"
 require "sc2ai/cli/ladderzip"
 
@@ -20,36 +22,36 @@ module Sc2
     desc "setup410", "Downloads and install SC2 v4.10"
     # downloads and install SC2 v4.10
     def setup410
-      puts " "
-      puts "This script sets up SC2 at version 4.10, which we use competitively."
-      puts "Press any key to continue..."
-      puts " "
-      $stdin.getch
+      say " "
+      say "This script sets up SC2 at version 4.10, which we use competitively."
+      say "Press any key to continue..."
+      ask ""
 
-      puts "You must accept the Blizzard® Starcraft® II AI and Machine Learning License at"
-      puts "https://blzdistsc2-a.akamaihd.net/AI_AND_MACHINE_LEARNING_LICENSE.html"
-      puts "It is PERMISSIVE and grants you freedoms over the standard EULA."
-      puts "We do not record this action, but depend on software goverend by that license to continue."
+      say "You must accept the Blizzard® Starcraft® II AI and Machine Learning License at"
+      say "https://blzdistsc2-a.akamaihd.net/AI_AND_MACHINE_LEARNING_LICENSE.html"
+      say "It is PERMISSIVE and grants you freedoms over the standard EULA."
+      say "We do not record this action, but depend on software goverend by that license to continue."
       puts 'If you accept, type "iagreetotheeula" (without quotes) and press ENTER to continue:'
 
       while $stdin.gets.chomp != "iagreetotheeula"
-        puts ""
+        say ""
         puts 'Type "iagreetotheeula" (without quotes) and press ENTER to continue:'
       end
-      puts ""
-      puts ""
-      puts "Great decision."
+      say ""
+      say ""
+      say "Great decision."
 
+      require "sc2ai"
       Async do
         Sc2.logger.level = :fatal
-        puts "SC2 will launch a blank window, be unresponsive, but download about 100mb in the background."
-        puts "Let it finish and close itself."
-        puts "Press ENTER if you understand."
-        $stdin.getch
+        say "SC2 will launch a blank window, be unresponsive, but download about 100mb in the background."
+        say "Let it finish and close itself."
+        say "Press ENTER if you understand."
+        ask ""
 
-        puts ""
-        puts ""
-        puts "This will only take a minute..."
+        say ""
+        say ""
+        say "This will only take a minute..."
 
         Sc2.config.version = nil
         client = Sc2::ClientManager.obtain(0)
@@ -68,22 +70,23 @@ module Sc2
         base_build = "75689"
         path = Sc2::Paths.executable(base_build: base_build)
         if path.include?(base_build) && Pathname(path).exist?
-          puts " "
-          puts "Success. Downloaded complete."
-          puts " "
+          say " "
+          say "Success. Download complete.", :green
+          say " "
         else
-          puts "Error. Slightly worrying, but no fear."
-          puts "To manually setup, grab the latest ladder maps and add them to your map folder."
-          puts "Grab the latest maps from https://aiarena.net/wiki/maps/"
-          puts "Detected map folder: #{Sc2::Paths.maps_dir}"
-          puts "Then, download any recent replay from https://aiarena.net/ and double click to launch"
+          say "Error. Slightly worrying, but no fear.", :red
+          say "To manually setup, grab the latest ladder maps and add them to your map folder."
+          say "Grab the latest maps from https://aiarena.net/wiki/maps/"
+          say "Detected map folder: #{Sc2::Paths.maps_dir}"
+          say "Then, download any recent replay from https://aiarena.net/ and double click to launch"
         end
 
+        observer.api.quit
         observer.disconnect
-        puts "Generating sc2ai.yml to always use 4.10..."
+        say "Generating sc2ai.yml to always use 4.10..."
         Sc2.config.config_file.write({"version" => "4.10"}.to_yaml.to_s)
-        puts ""
-        puts "Done. You're good to go."
+        say ""
+        say "Done. You're good to go.", :green
       ensure
         Sc2::ClientManager.stop(0)
       end
